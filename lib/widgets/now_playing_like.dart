ae59e87e464aaf24d39ccf06e711f629aa455e44
Takiyo Takahashi <takiyotakahashi@gmail.com>
2025-02-15 17:35:12 +0700

    feat: add now playing like widget

---
+++ lib/widgets/now_playing_like.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resonix/services/api_service.dart';
+
+class NowPlayingLike extends StatefulWidget {
+  final String? trackId;
+
+  const NowPlayingLike({super.key, required this.trackId});
+
+  @override
+  NowPlayingLikeState createState() => NowPlayingLikeState();
+}
+
+class NowPlayingLikeState extends State<NowPlayingLike> {
+  bool? isLiked;
+
+  @override
+  void initState() {
+    super.initState();
+    print("init bruh");
+    _fetchLikeStatus();
+  }
+
+  Future<void> _fetchLikeStatus() async {
+    print("isLiked: $isLiked");
+    if (widget.trackId == null) return;
+    print("hmm");
+    var response = await ApiService.trackLiked(widget.trackId!);
+    if (!mounted) return;
+    if (response != null) {
+      if (response["error"] != null) {
+        ApiService.returnError(context, response["error"]);
+        return;
+      }
+
+      setState(() => isLiked = response["liked"] == true);
+    } else {
+      await ApiService.returnTokenExpired(context);
+      return;
+    }
+  }
+
+  Future<void> _toggleLike() async {
+    if (widget.trackId == null) return;
+    var response = await ApiService.likeTrack(widget.trackId!);
+    if (!mounted) return;
+    if (response != null) {
+      if (response["error"] != null) {
+        ApiService.returnError(context, response["error"]);
+        return;
+      }
+      setState(() => isLiked = response["liked"] == true);
+    } else {
+      await ApiService.returnTokenExpired(context);
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      onPressed: isLiked == null ? null : _toggleLike,
+      child: Icon(
+        isLiked == true ? CupertinoIcons.heart_fill : CupertinoIcons.heart,
+        size: 30,
+        color: isLiked == true ? Colors.pinkAccent : Colors.white,
+      ),
+    );
+  }
+}
